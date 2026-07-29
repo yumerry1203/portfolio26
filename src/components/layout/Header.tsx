d1f84340f3e14379bdbb31a5eeccefe54c567c1d
yumerry1203 <moa3627@naver.com>
2026-07-29 18:01:48 +0900

    feat: 홈 화면 레이아웃과 Tailwind 디자인 토큰 추가

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,33 @@
+import logoBlack from '@/assets/images/logo-black.svg'
+
+const Header = () => {
+  return (
+    <>
+      <header className="flex items-center gap-32 h-40">
+        <img src={logoBlack} alt="" />
+        <div className="flex-1 h-px bg-black"></div>
+        <nav>
+          <ul className="flex items-center gap-40 font-heading text-lg text-black">
+            <li>
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <a href="#about">About Me</a>
+            </li>
+            <li>
+              <a href="#projects">Projects</a>
+            </li>
+            <li>
+              <a href="#skills">Skills</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  )
+};
+
+export default Header;
